c0cbed9b0c31c64d0e887b8cfe8dc09935baf6d4
Githab-capibara <rrrarrr37r@gmail.com>
2026-08-04 14:41:59 +0200

    feat(allowlist): add Nix support (#615)
    Co-authored-by: kite <lizhengfeng.lzf@alibaba-inc.com>

--- internal/config/allowlist/allowed_ext_test.go
+++ internal/config/allowlist/allowed_ext_test.go
@@ -48,6 +48,8 @@ func TestIsAllowedExt(t *testing.T) {
 		{".BICEP", true},
 		{".proto", true},
 		{".PROTO", true},
+		{".nix", true},
+		{".NIX", true},
 		{".hs", true},
 		{".HS", true},
 		{".lhs", true},
